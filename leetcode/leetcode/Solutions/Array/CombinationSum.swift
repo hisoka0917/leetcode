acca0ed7f2216840e169efcb3b6b31cc1be088ad
hisoka0917 <hisoka0917@gmail.com>
2021-07-13 22:06:05 -0400

    39. Combination Sum

---
+++ leetcode/leetcode/Solutions/Array/CombinationSum.swift
@@ -0,0 +1,53 @@
+//
+//  CombinationSum.swift
+//  leetcode
+//
+//  Created by Kai Wang on 7/13/21.
+//  Copyright © 2021 Pirate. All rights reserved.
+//
+
+import Foundation
+
+class CombinationSum {
+    func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
+        var res = [[Int]]()
+        combinationSumHelper(candidates: candidates, target: target, start: 0, curCombine: [], combinations: &res)
+        return res
+    }
+
+    func combinationSumHelper(candidates: [Int], target: Int, start: Int, curCombine: [Int], combinations: inout [[Int]]) {
+        if target < 0 {
+            return
+        } else if 0 == target {
+            combinations.append(curCombine)
+            return
+        } else {
+            var curCombine = curCombine
+            for i in start ..< candidates.count {
+                curCombine.append(candidates[i])
+                combinationSumHelper(candidates: candidates,
+                                     target: target - candidates[i],
+                                     start: i,
+                                     curCombine: curCombine,
+                                     combinations: &combinations)
+                curCombine.removeLast()
+            }
+        }
+    }
+
+    func test() {
+        let case1 = DataModel(candidates: [2,3,6,7], target: 7)
+        let case2 = DataModel(candidates: [2,3,5], target: 8)
+        let case3 = DataModel(candidates: [2], target: 1)
+        let case4 = DataModel(candidates: [1], target: 1)
+        let cases = [case1, case2, case3, case4]
+        for input in cases {
+            print(combinationSum(input.candidates, input.target))
+        }
+    }
+
+    struct DataModel {
+        var candidates: [Int]
+        var target: Int
+    }
+}
---
+++ leetcode/leetcode/Solutions/Array/CombinationSumII.swift
@@ -0,0 +1,16 @@
+//
+//  CombinationSumII.swift
+//  leetcode
+//
+//  Created by Kai Wang on 7/13/21.
+//  Copyright © 2021 Pirate. All rights reserved.
+//
+
+import Foundation
+
+class CombinationSumII {
+    func combinationSum2(_ candidates: [Int], _ target: Int) -> [[Int]] {
+        var res = [[Int]]()
+        return res
+    }
+}
