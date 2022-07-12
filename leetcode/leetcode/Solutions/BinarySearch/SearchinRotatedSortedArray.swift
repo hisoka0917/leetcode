6b3eea39c86b42c6d6f30c55bb8caa4b05649211
hisoka0917 <hisoka0917@gmail.com>
2022-07-12 16:56:06 -0400

    Organize

--- leetcode/leetcode/Solutions/BinarySearch/SearchinRotatedSortedArray.swift
+++ leetcode/leetcode/Solutions/BinarySearch/SearchinRotatedSortedArray.swift
@@ -82,30 +82,30 @@ class SearchinRotatedSortedArray {
 }
 
 // optimize
-//func search(_ nums: [Int], _ target: Int) -> Int {
-//        var left = 0
-//        var right = nums.count - 1
-//        while left <= right {
-//            let mid = (left + right) / 2
-//            if nums[mid] == target {
-//                return mid
-//            }
-//
-//            // valid left
-//            if nums[mid] >= nums[right] {
-//                if nums[left] <= target && target < nums[mid] {
-//                    right = mid - 1
-//                } else {
-//                    left = mid + 1
-//                }
-//            }  else {
-//                if nums[mid] < target && target <= nums[right] {
-//                    left = mid + 1
-//                } else {
-//                    right = mid - 1
-//                }
-//            }
-//        }
-//
-//        return -1
-//}
+func search(_ nums: [Int], _ target: Int) -> Int {
+        var left = 0
+        var right = nums.count - 1
+        while left <= right {
+            let mid = (left + right) / 2
+            if nums[mid] == target {
+                return mid
+            }
+
+            // valid left
+            if nums[mid] >= nums[right] {
+                if nums[left] <= target && target < nums[mid] {
+                    right = mid - 1
+                } else {
+                    left = mid + 1
+                }
+            } else {
+                if nums[mid] < target && target <= nums[right] {
+                    left = mid + 1
+                } else {
+                    right = mid - 1
+                }
+            }
+        }
+
+        return -1
+}
