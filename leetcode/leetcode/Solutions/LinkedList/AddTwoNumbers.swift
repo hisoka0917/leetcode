30fe70b05bcdc6f6a91593e99a40774fcdbaa667
hisoka0917 <hisoka0917@gmail.com>
2022-07-13 18:12:36 -0400

    2. Add Two Numbers

--- leetcode/leetcode/Solutions/LinkedList/AddTwoNumbers.swift
+++ leetcode/leetcode/Solutions/LinkedList/AddTwoNumbers.swift
@@ -18,26 +18,27 @@ class AddTwoNumbers {
         }
         var l1 = l1
         var l2 = l2
-        let header = ListNode(0)
+        let header: ListNode? = ListNode()
         var node = header
         var carry = 0
-        var sum = 0
         while l1 != nil || l2 != nil || carry > 0 {
-            let val1 = l1?.val ?? 0
-            let val2 = l2?.val ?? 0
-            sum = val1 + val2 + carry
-            carry = 0
-            if sum >= 10 {
-                carry = 1
-                sum -= 10
+            var sum = 0
+            if l1 != nil {
+                sum += l1!.val
             }
-            let newNode = ListNode.init(sum)
-            node.next = newNode
+            if l2 != nil {
+                sum += l2!.val
+            }
+            sum += carry
+            carry = sum / 10
+            sum %= 10
+
+            node?.next = ListNode(sum)
             l1 = l1?.next
             l2 = l2?.next
-            node = newNode
+            node = node?.next
         }
 
-        return header.next
+        return header?.next
     }
 }
